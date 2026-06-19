ad442e672349c3be1e7c7c230429ca7d27b3b32f
Koshmarjk <kostadacenko012@gmail.com>
2026-06-19 16:09:12 +0300

    Delete launch.bat

--- launch.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "%~dp0"
-
-:: Сборка если нужно
-dotnet build -c Debug -v quiet 2>nul
-
-:: Запуск exe напрямую — без пауз, без | more
-start "" "bin\Debug\net8.0-windows\HachBobAI.exe"
